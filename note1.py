55d63646fc21137267235e319b465cdc4b619b18
Daniel Rodrigues <61955775+dxnielsr@users.noreply.github.com>
2020-09-28 17:58:54 -0400

    declaracao e testes de variavel

---
+++ note1.py
@@ -0,0 +1,11 @@
+print("Hello World!")
+
+# Variáveis: armazenar informações na memória (RAM) para utilizar depois
+
+variavel = 1
+numero = 5
+nome = "Daniel"
+numero_decimal = 5.233
+vivo = True
+
+print(variavel, numero, nome, numero_decimal, vivo)
